907a836a539b11bb6ad5e30a75dbc610baa847aa
ricotama <ricotama4321@gmail.com>
2025-03-12 01:40:58 +0700

    pgwl acara 4

---
+++ resources/views/table.blade.php
@@ -0,0 +1,50 @@
+@extends('layout.template')
+
+@section('content')
+<div class="container mt-4">
+    <div class="card shadow-lg">
+        <div class="card-body">
+            <table class="table table-striped table-hover table-bordered text-center">
+                <thead class="table-dark">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Kelas</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Rico</td>
+            <td>23/522445/SV/23695</td>
+            <td>B</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pratama</td>
+            <td>23/522445/SV/23695</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Hartono</td>
+            <td>23/522445/SV/23695</td>
+            <td>B</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Mulyono</td>
+            <td>23/522445/SV/23695</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>fufufafa</td>
+            <td>23/522445/SV/23695</td>
+            <td>B</td>
+        </tr>
+    </tbody>
+</table>
+
+@endsection
